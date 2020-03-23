931e57f88e243192c134abc04f444d24efa289f9
Jeff Maughan <geophray@gmail.com>
2020-03-23 16:35:48 -0700

    Add trim to get rid of space in arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -6,22 +6,29 @@ var parseJSON = function(json) {
   //json = JSON string of objects (key-value pairs) and arrays
   var firstChar = json.charAt(0);
   //remove brackets/braces
-  var jsonString = json.slice(1,json.length-1);
+  var jsonString = json.slice(1, json.length - 1);
   //json string --> array
-  var jsonArr = jsonString.split(',');
+  var jsonArr;
+  if (jsonString.length === 0) {
+    jsonArr = [];
+  } else {
+    jsonArr = jsonString.split(',');
+  }
   //declare object
   var jsonObj = {};
-  if(firstChar === '[') {
-    if (jsonString.length === 0) {
-      return [];
-    } else {
-      return jsonArr;
+  if (firstChar === '[') {
+    // loop through array
+    for (var i = 0; i < jsonArr.length; i++) {
+      jsonArr[i] = jsonArr[i].trim();
+      jsonArr[i] = jsonArr[i].slice(1, jsonArr[i].length - 1);
     }
+    return jsonArr;
   } else if (firstChar === '{') {
     for (var i = 0; i < jsonArr.length; i++) {
       var pair = jsonArr[i].split(': ');
-      var key = pair[0].slice(1,pair[0].length-1);
-      var value = pair[1].slice(1,pair[1].length-1);
+      var key = pair[0].trim();
+      key = key.slice(1, key.length - 1);
+      var value = pair[1].slice(1, pair[1].length - 1);
       jsonObj[key] = value;
     }
   }
